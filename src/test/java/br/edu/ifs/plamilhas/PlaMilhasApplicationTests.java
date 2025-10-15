657d632c58ea8f9b5f48502f73ec7fb446198747
gildeonbs <gildeon23@gmail.com>
2025-10-14 23:21:11 -0300

    Upload do projeto Spring Boot

---
+++ src/test/java/br/edu/ifs/plamilhas/PlaMilhasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifs.plamilhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaMilhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
